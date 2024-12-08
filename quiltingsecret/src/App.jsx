4aae512213031577cfcdd381f3ef98edfbc4ff63
EnzoPaalman <35348@ma-web.nl>
2024-12-08 14:05:25 +0100

    webproject

---
+++ quiltingsecret/src/App.jsx
@@ -0,0 +1,21 @@
+import './App.css'
+
+function Header(){
+    return (
+    <>
+      
+      <nav>
+        <img src="" alt="" />
+        <h1>A Quilting Secret</h1>
+        <ul className='list-items'>
+          <li>Shop</li>
+          <li>Contact</li>
+          <li>Info</li>
+        </ul>
+      </nav>
+
+    </>
+  );
+}
+
+export default Header
